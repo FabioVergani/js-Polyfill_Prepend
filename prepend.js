b24c7363c6a310bd640a3861757333f031cb9feb
Fabio Vergani <fabio.vergani.1975@gmail.com>
2017-05-25 10:15:38 +0200

    Update prepend.js

--- prepend.js
+++ prepend.js
@@ -1,10 +1,10 @@
 (function(w){
-	var o=w.Element.prototype,p="prepend";
-	if(!((p in o) && o.hasOwnProperty(p))){
+	var n=w.Node,o=w.Element.prototype,p="prepend";
+	if(!((p in o)&&o.hasOwnProperty(p))){
 		Object.defineProperty(o,p,{
 			value:function(){
 				var d=w.document,a=d.createDocumentFragment(),m=arguments,l=m.length,i=0,b;
-				while(i<l){b=m[i];a.appendChild((b instanceof Node)?b:d.createTextNode(String(b)));++i};
+				while(i<l){b=m[i];a.appendChild((b instanceof n)?b:d.createTextNode(String(b)));++i};
 				(b=this).insertBefore(a,b.firstChild);
 			}
 		})
